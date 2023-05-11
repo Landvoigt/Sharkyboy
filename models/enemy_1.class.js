c1083f85642ae5ce8547c4776e1d31c7d1d9dd18
Landvoigt <tim.voigt@ik.me>
2023-05-11 14:55:54 +0200

    first commit

---
+++ models/enemy_1.class.js
@@ -0,0 +1,6 @@
+class Enemy_1 extends MovableObject {  // Pufferfish
+    constructor(){
+        super().loadImage('../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
+    }
+
+}
---
+++ models/world.class.js
@@ -0,0 +1,13 @@
+class World {
+    character = new Character();
+    enemies = [
+        new Enemy_1(),
+        new Enemy_1(),
+        new Enemy_1()
+    ];
+
+
+    draw() {
+
+    }
+}
