486a7e43f527e717d1d340b69dff335e06310adb
Landvoigt <tim.voigt@ik.me>
2023-06-07 14:53:16 +0200

    enemy transition

---
+++ load/enemies.js
@@ -0,0 +1,29 @@
+const PUFFER_FISH_RED_SWIM_IMG = [
+    '../img/enemies/puffer_fish_red/swim/(1).png',
+    '../img/enemies/puffer_fish_red/swim/(2).png',
+    '../img/enemies/puffer_fish_red/swim/(3).png',
+    '../img/enemies/puffer_fish_red/swim/(4).png',
+    '../img/enemies/puffer_fish_red/swim/(5).png'
+];
+
+const PUFFER_FISH_RED_TRANSITION_IMG = [
+    '../img/enemies/puffer_fish_red/transition/(1).png',
+    '../img/enemies/puffer_fish_red/transition/(2).png',
+    '../img/enemies/puffer_fish_red/transition/(3).png',
+    '../img/enemies/puffer_fish_red/transition/(4).png',
+    '../img/enemies/puffer_fish_red/transition/(5).png'
+];
+
+const PUFFER_FISH_RED_ATTACK_IMG = [
+    '../img/enemies/puffer_fish_red/attack/(1).png',
+    '../img/enemies/puffer_fish_red/attack/(2).png',
+    '../img/enemies/puffer_fish_red/attack/(3).png',
+    '../img/enemies/puffer_fish_red/attack/(4).png',
+    '../img/enemies/puffer_fish_red/attack/(5).png'
+];
+
+const PUFFER_FISH_RED_DEAD_IMG = [
+    '../img/enemies/puffer_fish_red/dead/(1).png',
+    '../img/enemies/puffer_fish_red/dead/(2).png',
+    '../img/enemies/puffer_fish_red/dead/(3).png'
+];
--- models/enemy_1.class.js
+++ models/enemy_1.class.js
@@ -9,21 +9,22 @@ class Enemy_1 extends MovableObject {  // Pufferfish
         width: 12,
         height: 42,
     };
-    SWIMMING_IMG = [
-        '../img/enemies/puffer_fish_red/swim/(1).png',
-        '../img/enemies/puffer_fish_red/swim/(2).png',
-        '../img/enemies/puffer_fish_red/swim/(3).png',
-        '../img/enemies/puffer_fish_red/swim/(4).png',
-        '../img/enemies/puffer_fish_red/swim/(5).png'
+    transitionAnimationCount = 0;
+    transitionDone = false;
 
-    ];
     constructor() {
-        super().loadImage(this.SWIMMING_IMG[0]);
-        this.loadImages(this.SWIMMING_IMG);
+        super().loadImage(PUFFER_FISH_RED_SWIM_IMG[0]);
+        this.load();
+        this.x = 1500 + Math.random() * 1000;  // Number between 200 and 700
+        this.speed = 0.2 + Math.random() * 0.6;
+        this.animate();
+    }
 
-        this.x = 750 + Math.random() * 1000;  // Number between 200 and 700
-        this.speed = 0.2 + Math.random() * 0.5;
-        // this.animate();
+    load() {
+        this.loadImages(PUFFER_FISH_RED_SWIM_IMG);
+        this.loadImages(PUFFER_FISH_RED_TRANSITION_IMG);
+        this.loadImages(PUFFER_FISH_RED_ATTACK_IMG);
+        this.loadImages(PUFFER_FISH_RED_DEAD_IMG);
     }
 
     animate() {
@@ -32,8 +33,39 @@ class Enemy_1 extends MovableObject {  // Pufferfish
         }, 1000 / 60);
 
         setInterval(() => {
-            this.playAnimation(this.SWIMMING_IMG);
-        }, 200);
+            if (this.characterNearby() && this.transitionInProgress()) {
+                if (this.transitionStarts()) {
+                    this.currentImage = 0;
+                }
+                if (this.transitionEnded()) {
+                    this.transitionDone = true;
+                }
+                this.playAnimation(PUFFER_FISH_RED_TRANSITION_IMG);
+                this.transitionAnimationCount++;
+            } else {
+                if (this.transitionDone) {
+                    this.playAnimation(PUFFER_FISH_RED_ATTACK_IMG);
+                }
+                else {
+                    this.playAnimation(PUFFER_FISH_RED_SWIM_IMG);
+                }
+            }
+        }, 160);
     }
 
+    characterNearby() {
+        return characterPosition + 1000 > this.x;
+    }
+
+    transitionStarts() {
+        return this.transitionAnimationCount == 0;
+    }
+
+    transitionInProgress() {
+        return this.transitionAnimationCount <= 4;
+    }
+
+    transitionEnded() {
+        return this.characterNearby() && this.transitionAnimationCount == 4;
+    }
 }
